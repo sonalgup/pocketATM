332389b4d75b6997ee310a59e54c65e814003ada
Varun <gpt.varun.24@gmail.com>
2020-11-25 11:51:55 +0530

    first commit

---
+++ src/main/java/com/PocketATM/PocketAtmApplication.java
@@ -0,0 +1,13 @@
+package com.PocketATM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocketAtmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocketAtmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/PocketATM/PocketAtmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PocketATM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocketAtmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
